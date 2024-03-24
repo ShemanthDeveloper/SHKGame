aa9a7d09e75d30ecdc2d281b66bfdc9851ca5d40
hemanth <hemanthsudhani36@gmail.com>
2024-03-24 17:13:17 +0530

    findNumber Functinality

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React,{Component} from "react"
+import FindNextNumber from "./components/FindNextNumber"
+import "./App.css"
+
+class App extends Component{
+  render(){
+    return(
+      <>
+      <FindNextNumber/>
+      </>
+    )
+  }
+
+}
+
+export default App
